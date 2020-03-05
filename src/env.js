e8f6d72bc9857667d3ad341a694d25edbe3d7936
TLTL1025 <1057798521@qq.com>
2020-03-05 17:18:51 +0800

    环境变量cors jsonp

---
+++ src/env.js
@@ -0,0 +1,23 @@
+//环境变量的统一配置，封装成模块
+//适用于 cros和jsonp的时候
+let baseURL;
+
+switch (process.env.NODE_ENV) {//nodejs中有个process进程,获取当前nodejs进程里面的一些环境变量，最终会写入nodejs
+    case 'development':
+        baseURL = 'http://dev-mall-pre.springboot.cn/api';
+        break;
+    case 'test':
+        baseURL = 'http://test-mall-pre.springboot.cn/api';
+        break;
+    case 'production':
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+    default:
+        baseURL = 'http://mall-pre.springboot.cn/api';
+        break;
+
+}
+
+export default {
+    baseURL
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
+import App from './App.vue'
 import store from './store'
+import env from './env'
 
+//发请求的时候设置一些基础值,根据前端的跨域方式做调整
+//当为接口代理时，域名是一样的，习惯叫api,转发的时候也可以切掉
+axios.defaults.baseURL='/api';
+axios.defaults.timeout=8000;
+//根据环境变量获取不同的请求地址
+axios.defaults.baseURL=env.baseURL;
+console.log(env.baseURL)
+
+//注：请求拦截可用request.use
+
+//拦截器(接口错误拦截)
+axios.interceptors.response.use(function(response){
+  let res=response.data;
+  if(res.status==0){
+    return res.data;
+  }else if(res.status==10){//未登陆的状态码==10
+    window.location.href='/#/login' //跳转到登录页面,#:哈希路由
+  }else{
+    alert(res.msg);
+  }
+})
+
+
+//注册
+Vue.use(VueAxios,axios);//发请求的时候就可以用this来调用了
 Vue.config.productionTip = false
 
 new Vue({
